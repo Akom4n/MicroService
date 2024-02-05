dbafe1201f44709efc13e653f0cf6f6987394c0e
Vinicius Silva <xdenik13@gmail.com>
2024-02-04 21:56:30 -0300

    Mudança de nomes

--- src/main/java/com/ms/user/UserApplication.java
+++ src/main/java/com/ms/user/UserApplication.java
@@ -1,12 +1,13 @@
-package com.den.mservice;
+package com.ms.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MserviceApplication {
+public class UserApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MserviceApplication.class, args);
+		SpringApplication.run(UserApplication.class, args);
 	}
+
 }
--- src/test/java/com/ms/user/UserApplicationTests.java
+++ src/test/java/com/ms/user/UserApplicationTests.java
@@ -1,10 +1,10 @@
-package com.den.mservice;
+package com.ms.user;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MserviceApplicationTests {
+class UserApplicationTests {
 
 	@Test
 	void contextLoads() {
